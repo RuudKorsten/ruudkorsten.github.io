6e3eb59295f1e031d9bfccb129590807795122ac
RuudKorsten <r.j.w.korsten@tilburguniversity.edu>
2025-02-27 16:36:16 +0100

    Updated game - 2025-02-27 16:36:16

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.error('Game generation failed');
+
+const container = document.getElementById('game-container');
+container.innerHTML = '<p>Click the box to score points!</p>';
+const box = document.createElement('div');
+box.style.width = '50px';
+box.style.height = '50px';
+box.style.backgroundColor = 'red';
+box.style.position = 'absolute';
+box.style.top = '50%';
+box.style.left = '50%';
+box.style.transform = 'translate(-50%, -50%)';
+container.appendChild(box);
+let score = 0;
+box.addEventListener('click', () => {
+    score++;
+    alert('Score: ' + score);
+});
